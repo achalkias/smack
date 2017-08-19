c989827cadbf23b0541faa3c85d10f33e3b8e895
Apostolos Chalkias <achalkias@hotmail.com>
2017-08-20 01:43:58 +0300

    Message timestamp completed

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -27,6 +27,24 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(componets: message.userAvatarColor)
+        
+        //Unrwap the time
+        guard var isoDate = message.timeStamp else { return }
+        //Remove the miliseconds
+        let end  = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormater = ISO8601DateFormatter()
+        let chatDate = isoFormater.date(from: isoDate.appending("Z"))
+        
+        let newFormater = DateFormatter()
+        newFormater.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate  = chatDate {
+            let finalDate = newFormater.string(from: finalDate)
+            timestampLbl.text = finalDate
+        }
+        
     }
     
 }
